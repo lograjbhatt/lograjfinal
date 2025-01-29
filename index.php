82df34985ee6946dc84a14472c3ad4e4cbb81be1
apurwa <apurwa@gmail.com>
2025-01-29 14:06:40 +0545

    new homepage image

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo '<h1>सुन्दर शुदुरपश्चिम प्रदेश !</h1>';
 echo '<h2>तेसमा पनि शुक्लाफाँटा झनै राम्रो !</h1>';
-echo'<img src="mnr.jpg"width="700">';
+echo'<img src="shukla.jpg"width="700">';
 ?>
